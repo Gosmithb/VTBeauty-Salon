55af5c6582b1f39566a9328c24162f7485c3ab40
Bryan <bryangodinez00@gmail.com>
2022-10-05 18:38:12 -0600

    Rutas agregadas, navegacion lista

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import Navbar from './components/Navbar';
 import App from './App'
 import './index.css'
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+    <Navbar />
     <App />
   </BrowserRouter>
 )
