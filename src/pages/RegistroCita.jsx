2f6fdb59937a73dbffd397de8265090732f85f10
Bryan <bryangodinez00@gmail.com>
2022-10-26 10:12:14 -0600

    Corregido campo select de registro de citas

--- src/pages/RegistroCita.jsx
+++ src/pages/RegistroCita.jsx
@@ -1,15 +1,15 @@
 
 const RegistroCita = () => {
   return (
-    <div class="flex items-center justify-center p-12">
-      <div class="mx-auto w-full max-w-[550px]">
+    <div className="flex items-center justify-center p-12">
+      <div className="mx-auto w-full max-w-[550px]">
         <form action="" method="POST">
-          <div class="-mx-3 flex flex-wrap">
-            <div class="w-full px-3 sm:w-1/2">
-              <div class="mb-5">
+          <div className="-mx-3 flex flex-wrap">
+            <div className="w-full px-3 sm:w-1/2">
+              <div className="mb-5">
                 <label
-                  for="fName"
-                  class="mb-3 block text-base font-medium text-[#07074D]">
+                  htmlFor="fName"
+                  className="mb-3 block text-base font-medium text-[#07074D]">
                   Nombre
                 </label>
                 <input
@@ -17,15 +17,15 @@ const RegistroCita = () => {
                   name="fName"
                   id="fName"
                   placeholder="Nombre"
-                  class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                  className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                 />
               </div>
             </div>
-            <div class="w-full px-3 sm:w-1/2">
-              <div class="mb-5">
+            <div className="w-full px-3 sm:w-1/2">
+              <div className="mb-5">
                 <label
-                  for="lName"
-                  class="mb-3 block text-base font-medium text-[#07074D]">
+                  htmlFor="lName"
+                  className="mb-3 block text-base font-medium text-[#07074D]">
                   Apellidos
                 </label>
                 <input
@@ -33,62 +33,59 @@ const RegistroCita = () => {
                   name="lName"
                   id="lName"
                   placeholder="Apellidos"
-                  class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                  className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                 />
               </div>
             </div>
           </div>
-          <div class="mb-5">
+
+          <div className="mb-5">
             <label
-              for="guest"
-              class="mb-3 block text-base font-medium text-[#07074D]">
+              htmlFor="guest"
+              className="mb-3 block text-base font-medium text-[#07074D]">
               Servicio Requerido
             </label>
-            <div class="w-full md:w-1/3 px-3">
-              <div class="relative">
-                <select class="w-full appearance-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" id="grid-state">
-                  <option>Seleccione un servicio...</option>
-                  <option>Corte de cabello</option>
-                  <option>Pintura de cabello</option>
-                  <option>Manicura</option>
-                  <option>Pedicura</option>
-                </select>
-                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                  <svg class="fill-current h-4 w-4"  viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-                </div>
-              </div>
+            <div class="max-w-2xl mx-auto">
+                <select id="services" class="rounded-md border border-[#e0e0e0] font-medium text-[#6B7280] font-medium text-base rounded-lg block w-full py-3 px-6 focus:border-[#6A64F1]">
+                <option selected>Selecciona un servicio...</option>
+                <option value="CDC">Corte de cabello</option>
+                <option value="PDC">Pintura de cabello</option>
+                <option value="MA">Manicura</option>
+                <option value="PE">Pedicura</option>
+              </select>
             </div>
 
           </div>
 
-          <div class="-mx-3 flex flex-wrap">
-            <div class="w-full px-3 sm:w-1/2">
-              <div class="mb-5">
+
+          <div className="-mx-3 flex flex-wrap">
+            <div className="w-full px-3 sm:w-1/2">
+              <div className="mb-5">
                 <label
-                  for="date"
-                  class="mb-3 block text-base font-medium text-[#07074D]">
+                  htmlFor="date"
+                  className="mb-3 block text-base font-medium text-[#07074D]">
                   Fecha
                 </label>
                 <input
                   type="date"
                   name="date"
                   id="date"
-                  class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                  className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                 />
               </div>
             </div>
-            <div class="w-full px-3 sm:w-1/2">
-              <div class="mb-5">
+            <div className="w-full px-3 sm:w-1/2">
+              <div className="mb-5">
                 <label
-                  for="time"
-                  class="mb-3 block text-base font-medium text-[#07074D]">
+                  htmlFor="time"
+                  className="mb-3 block text-base font-medium text-[#07074D]">
                   Hora
                 </label>
                 <input
                   type="time"
                   name="time"
                   id="time"
-                  class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                  className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                 />
               </div>
             </div>
@@ -96,7 +93,7 @@ const RegistroCita = () => {
 
           <div>
             <button
-              class="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white outline-none">
+              className="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white outline-none">
               Crear cita
             </button>
           </div>
