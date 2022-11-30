dc43ae553ef853eb87233241d6ae93a30f95f9e8
Bryan <bryangodinez00@gmail.com>
2022-11-29 19:42:36 -0700

    Se agregaron validaciones de registro de cita

--- src/components/InputComponent.jsx
+++ src/components/InputComponent.jsx
@@ -1,19 +1,16 @@
 import { faCircleCheck, faTimesCircle } from "@fortawesome/free-solid-svg-icons";
 
-const InputComponent = ({ estado, setEstado, tipo, placeholder, name, leyendaError, expresionRegular, funcion }) => {
+const InputComponent = ({ estado, setEstado, tipo, placeholder, name, leyendaError, expresionRegular, funcion, classProps }) => {
     const handlesetEstado = (e) => {
         setEstado({ ...estado, campo: e.target.value });
     };
 
     const handleValidacion = () => {
         if (expresionRegular) {
-            console.log('Si entra a validacion');
             if (expresionRegular.test(estado.campo)) {
                 setEstado({ ...estado, valido: 'true' });
-                console.log('Valido en true');
             } else {
                 setEstado({ ...estado, valido: 'false' });
-                console.log('Valido en false');
             }
         }
 
@@ -39,7 +36,7 @@ const InputComponent = ({ estado, setEstado, tipo, placeholder, name, leyendaErr
                 onKeyUp={handleValidacion}
                 onBlur={handleOnBlur}
                 valido={estado.valido}
-                className="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0"
+                className={classProps}
             />
             {/* Falta icono de validacion en labels */}
             <label
@@ -47,14 +44,11 @@ const InputComponent = ({ estado, setEstado, tipo, placeholder, name, leyendaErr
                 valido={estado.valido}>
             </label>
             {estado.valido === 'false' &&
-                <div class="mt-1 rounded-md flex items-center bg-blue-500 text-white text-xs font-bold px-4 py-1" role="alert">
-                    <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z"/></svg>
+                <div className="mt-1 rounded-md flex items-center bg-blue-500 text-white text-xs font-bold px-4 py-1" role="alert">
+                    <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z"/></svg>
                     <p>{leyendaError}</p>
                 </div>
             }
-            {/* <p valido={estado.valido}>
-                {estado.valido === 'true' ? Valido : {leyendaError}}
-            </p> */}
         </div>
     )
 }
